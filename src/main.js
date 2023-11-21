df61de33d2825b26a5a4c05333254f59fcc812b6
Júnior Nascimento <junior.nascm@gmail.com>
2023-11-21 16:06:24 -0300

    adding jumping and obstacle

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import CannonDebugger from 'cannon-es-debugger'
 const scene = new THREE.Scene();
 
 let player;
+let obstacle;
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 10, 20);
@@ -14,13 +15,12 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-window.addEventListener('resize', onWindowResize);
+window.addEventListener('keydown', event => {
+  if(event.key === " ") {
+    jump();
+  }
+});
 
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
 
 const world = new CANNON.World();
 world.gravity.set(0, -9.82, 0);
@@ -68,7 +68,7 @@ function initCannonWorld() {
 
 function initPlayer() {
   const geometry = new THREE.CapsuleGeometry(1.5, 1.5, 8, 32);
-  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
 
   const mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
@@ -84,11 +84,38 @@ function initPlayer() {
   player = { mesh, body };
 }
 
+function initObstacle() {
+  const geometry = new THREE.BoxGeometry(25, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const shape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5));
+  const body = new CANNON.Body({ mass: 1 });
+  body.addShape(shape);
+  body.position.set(0, 4, -80);
+  body.angularVelocity.set(0, 0, 0);
+  body.velocity.set(0, 0, 100);
+  body.angularDamping = 0;
+  world.addBody(body);
+
+  obstacle = { mesh, body };
+}
+
 const cannonDebugger = new CannonDebugger(scene, world, {});
 
 initThreeScene();
 initCannonWorld();
 initPlayer();
+initObstacle();
+
+function jump() {
+  // prevent player to jump in the air
+  // This is kind bad, but nothing to worry about it
+  // TODO: Refactor this
+  if (player.body.position.y > 4) return;
+  player.body.velocity.y = 10;
+}
 
 function animate() {
   requestAnimationFrame(animate);
@@ -97,6 +124,11 @@ function animate() {
 
   player.mesh.position.copy(player.body.position)
   player.mesh.quaternion.copy(player.body.quaternion)
+
+  obstacle.mesh.position.copy(obstacle.body.position)
+  obstacle.mesh.quaternion.copy(obstacle.body.quaternion)
+
+
 
   renderer.render(scene, camera);
 }
